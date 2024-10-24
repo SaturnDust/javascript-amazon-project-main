48b42ada2b3b29e01b9e0bd2335a82267656b4bf
SaturnDust <razzanindrana26@gmail.com>
2024-10-24 14:40:35 +0700

    Delete product from HTML when click delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -16,7 +16,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${productQuery.id}">
           <div class="delivery-date">
             Delivery date: Tuesday, June 21
           </div>
@@ -95,5 +95,8 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
     const productId = link.dataset.productId;
     
     removeFromCart(productId);
+
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.remove();
   });
 });
